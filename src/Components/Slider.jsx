cec093e0a7c91802a7a1d98638f6e2fb38aa49a1
Umut Şenol <umut_senol@outlook.com>
2025-05-25 01:51:13 +0300

    scrolling movie images with the arrow key

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../Services/GlobalApi'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 function Slider() {
     const [movieList,setMovieList]=useState([]);
-
+    const elementRef = useRef();
     useEffect(() => {
         getTrendingMovies();
     },[])
@@ -21,12 +22,30 @@ function Slider() {
         })
     }
 
+    const sliderLeft = (element) => {
+        element.scrollLeft = element.scrollLeft - element.clientWidth + 110;
+    }
+    const sliderRight = (element) => {
+        element.scrollLeft = element.scrollLeft + element.clientWidth - 110;
+    }
+
   return (
-    <div className='flex overflow-x-auto w-full px-16 py-4 max-md:scrollbar-hide'>
-        {movieList.map((item) => (
-           <img src = {IMAGE_BASE_URL+item.backdrop_path}
-           className='min-w-full h-[350px] object-cover object-left-top mr-5 rounded-md'/>
+    <div>
+        <HiChevronLeft className="hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[150px] cursor-pointer " 
+        onClick={()=>sliderLeft(elementRef.current)}/>
+        <HiChevronRight className='hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[150px] cursor-pointer right-0' 
+        onClick={()=>sliderRight(elementRef.current)}/>
+   
+    <div className='flex overflow-x-auto px-16 py-4 scrollbar-hide' ref={elementRef}> 
+        {movieList.map((item)=>(
+            <img src={IMAGE_BASE_URL+item.backdrop_path} 
+            className='min-w-full  md:h-[310px] object-cover
+            object-left-top mr-5 rounded-md hover:border-[4px]
+            border-gray-400 transition-all duration-100 ease-in'/>
         ))}
+    </div>
     </div>
   )
 }
